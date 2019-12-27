bf1bccd8fbba284c5a7bb8442aa433da2173a922
Marcin Rataj <lidel@lidel.org>
2019-12-27 12:44:15 +0100

    fix: window.ipfs test requires files.get
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- src/providers/window-ipfs.js
+++ src/providers/window-ipfs.js
@@ -4,7 +4,11 @@ const PROVIDERS = require('../constants/providers')
 
 async function tryWindow ({ root, permissions, connectionTest }) {
   if (root.ipfs) {
-    // https://github.com/ipfs-shipyard/ipfs-companion/issues/589
+    permissions = permissions || {}
+    permissions.commands = permissions.commands || []
+    // files.get is required for testing if API works
+    if (!permissions.commands.includes('files.get')) permissions.commands.push('files.get')
+    // try window.ipfs.enable first: https://github.com/ipfs-shipyard/ipfs-companion/issues/589
     const ipfs = typeof root.ipfs.enable === 'function'
       ? await root.ipfs.enable(permissions)
       : root.ipfs
